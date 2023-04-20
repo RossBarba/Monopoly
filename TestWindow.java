fb07fddd935a7b5b4f1dac31d91bae165e81b61f
Ross Barba <105376681+RossBarba@users.noreply.github.com>
2023-04-20 15:06:05 -0500

    Update TestWindow.java

--- TestWindow.java
+++ TestWindow.java
@@ -12,18 +12,23 @@ import javax.swing.BoxLayout;
 import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Image;
+import javax.swing.JLabel;
 
 public class TestWindow extends JFrame {
 	private JTextField textField;
@@ -34,52 +39,41 @@ public class TestWindow extends JFrame {
 		getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("Barter");
-		btnNewButton.setBounds(524, 380, 111, 21);
+		btnNewButton.setBounds(1232, 576, 182, 43);
 		getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Property Management");
-		btnNewButton_1.setBounds(524, 413, 111, 21);
+		btnNewButton_1.setBounds(1232, 629, 182, 48);
 		getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Roll");
-		btnNewButton_2.setBounds(524, 444, 111, 21);
+		btnNewButton_2.setBounds(1232, 687, 182, 43);
 		getContentPane().add(btnNewButton_2);
 		
 		JTextArea textArea = new JTextArea();
-		textArea.setBounds(10, 378, 490, 56);
+		textArea.setBounds(766, 8, 427, 629);
 		getContentPane().add(textArea);
 		
 		textField = new JTextField();
-		textField.setBounds(10, 445, 490, 19);
+		textField.setBounds(766, 656, 427, 70);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		
 		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(524, 10, 111, 360);
+		textArea_1.setBounds(1232, 8, 182, 543);
 		getContentPane().add(textArea_1);
 		
-		Gpanel Board = new Gpanel();
-		Board.setBounds(10, 10, 490, 360);
-		getContentPane().add(Board);
+		
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\barba\\workspace\\WIndowTest\\src\\Window\\Monopoly700.jpg"));
+		lblNewLabel.setBounds(10, 10, 715, 715);
+		getContentPane().add(lblNewLabel);
+		
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	class Gpanel extends JPanel {
 	
-		public void paintComponent(Graphics g) {
-			super.paintComponents(g);
-
-			try {
-		        img = ImageIO.read(new File("C:/Users/barba/Downloads/OrigB.gif"));
-		    } catch (IOException e) {
-		        System.out.println("no image");
-		    }
-		    
-		    
-	}
-	
-	}
 	public static void main(String[] args) {
 		
 		TestWindow x = new TestWindow();
